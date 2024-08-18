14fda05562a6ac2179ce38c8eeb6070b5ab260cd
sjaturner <sjaturner@googlemail.com>
2024-08-18 10:53:47 +0100

    that looks like the simplest tokenizer

--- arrange.c
+++ arrange.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+struct link
+{
+    struct link *link;
+    char *tag;
+};
+
+int recurse(int argc, char *argv[], struct link *link)
+{
+}
+
 int main(int argc, char *argv[])
 {
+    char token[0x100] = { };
+    while(scanf("%s", &token) != EOF)
+    {
+        printf("token:%s\n", token);
+    }
     return 0;
 }
